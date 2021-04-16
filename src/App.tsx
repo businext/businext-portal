01941b625b1d2de30a3e9ae93c715506bf60b8cb
Justin Xu <xu.justin.j@gmail.com>
2021-04-15 21:56:07 -0400

    Convert indentation to tabs

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,22 @@ import Header from "./components/Header";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 const App = () => {
-  return (
-    <div className="app">
-      <BrowserRouter>
-        <Switch>
-          <Route
+	return (
+		<div className="app">
+			<BrowserRouter>
+				<Switch>
+					<Route
 						path="/" exact
 						component={() => <Header title="Welcome"/>}
 					/>
-          <Route
-            path="/insights" exact
-            component={() => <Header title="Search Insights"/>}
-          />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+					<Route
+						path="/insights" exact
+						component={() => <Header title="Search Insights"/>}
+					/>
+				</Switch>
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default App;
